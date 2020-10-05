00e75783f2dd7c78a442e3b910dbf7eaff0e0676
CamCommand <48034712+CamCommand@users.noreply.github.com>
2020-10-05 16:08:50 -0400

    Add files via upload

---
+++ Morning.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+new=2;
+
+url="https://www.serebii.net/";
+url2="https://www.youtube.com/";
+url3="https://jojo-portal.com/";
+url4="https://www.reddit.com/";
+url5="https://www.linkedin.com/feed/";
+
+print("Good Luck This Morning");
+
+webbrowser.open(url,new=new);
+webbrowser.open(url2,new=new);
+webbrowser.open(url3,new=new);
+webbrowser.open(url4,new=new);
+webbrowser.open(url5,new=new);
