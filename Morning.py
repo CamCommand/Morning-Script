ade19a3e960912421bf3aab0b4913b231bcc2a32
CamCommand <48034712+CamCommand@users.noreply.github.com>
2020-10-20 16:26:08 -0400

    Update Morning.py

--- Morning.py
+++ Morning.py
@@ -1,26 +1,22 @@
 import webbrowser
 import time
 
-new=2;
+new=2
 
-url0="https://www.youtube.com/watch?v=WsiiVixyXsk";
+url0="https://www.youtube.com/watch?v=WsiiVixyXsk"
 #play me
-url="https://www.serebii.net/";
-url2="https://www.youtube.com/";
-url3="https://drive.google.com/";
-url4="https://www.reddit.com/";
-url5="https://www.linkedin.com/feed/";
+url3="https://drive.google.com/"
+url4="https://www.reddit.com/"
+url5="https://www.linkedin.com/feed/"
 
-print("Good Luck This Morning");
-webbrowser.open(url0,new=new);
+print("Good Luck This Morning")
+webbrowser.open(url0,new=new)
 time.sleep(15)
-webbrowser.open(url,new=new);
+webbrowser.open(url2,new=new)
 time.sleep(15)
-webbrowser.open(url2,new=new);
+webbrowser.open(url3,new=new)
 time.sleep(15)
-webbrowser.open(url3,new=new);
+webbrowser.open(url4,new=new)
 time.sleep(15)
-webbrowser.open(url4,new=new);
-time.sleep(15)
-webbrowser.open(url5,new=new);
+webbrowser.open(url5,new=new)
 
